72e01edb6b644432d63d486fd8cd5afb2076974f
veganaize <veganaize@users.noreply.github.com>
2020-09-09 18:42:51 -0700

    Remove redundant code

--- webserver.js
+++ webserver.js
@@ -1,35 +1,33 @@
 var http = require('http');
 var fs = require('fs');
 
-var server = http.createServer(function(request, response) {
-        request.url = 'public_html/'+ request.url;
-        fs.stat(request.url, function(bad_url, file) {
-            if (bad_url) {
-                response.statusCode = 404;
-                response.end();
-            }
-            else if (file.isDirectory()) {                
-                fs.readFile(request.url +'/index.html', function(bad_filename, file_content) {
-                    if (bad_filename) {
-                        response.statusCode = 404;
-                        response.end();
-                    } else {
-                        response.statusCode = 200;
-                        response.setHeader('Content-Type', 'text/html');
-                        response.end(file_content);
-                    }
-                });
-            } else {
-                fs.readFile(request.url, function(bad_filename, file_content) {
-                    if (bad_filename) {
-                        response.statusCode = 404;
-                        response.end();
-                    } else {
-                        response.statusCode = 200;
-                        response.setHeader('Content-Type', 'text/html');
-                        response.end(file_content);
-                    }
-                });
-            }
-        });  
+http.createServer(function(request, response) {
+    request.url = 'public_html'+ request.url;
+    fs.stat(request.url, function(bad_url, url_stat) {
+        if (bad_url) send_404();
+        else {
+            if (url_stat.isDirectory()) request.url += '/index.html';
+            fs.readFile(request.url, check_and_send_file);
+        }
+    });
+        
+    function check_and_send_file(bad_filename, file_content) {
+        if (bad_filename) {
+            send_404();
+        } else {
+            send_file_content(file_content);
+        }
+    }
+
+    function send_404() {
+        response.statusCode = 404;
+        response.end();
+    }
+
+    function send_file_content(file_content) {
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'text/html');
+        response.end(file_content);
+    }
+
 }).listen(80, function(){ console.log('Node.js webserver is running!  [CTRL]+[C] to stop!') });
