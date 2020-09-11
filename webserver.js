1773d332d96fc62f09d27d6b8488456a486bbfa4
veganaize <veganaize@users.noreply.github.com>
2020-09-10 17:42:26 -0700

    Improve trailing slash handling

--- webserver.js
+++ webserver.js
@@ -6,14 +6,13 @@ http.createServer(function(request, response) {
     fs.stat(prefixed_url, function(bad_url, url_stat) {
         if (bad_url) respond(404);
         else {
-            if (url_stat.isDirectory()) {
-                if (request.url.slice(-1) !== '/') {
-                    response.setHeader('Location', request.url +'/');
-                    respond(301);
-                }
-                
-                prefixed_url += '/index.html';
+            if (url_stat.isDirectory() && request.url.slice(-1) !== '/') {
+                response.setHeader('Location', request.url +'/');
+                respond(301);
+                return;
             }
+            
+            if (prefixed_url.slice(-1) === '/') prefixed_url += 'index.html';
             fs.readFile(prefixed_url, function(bad_filename, file_content) {
                 if (bad_filename) respond(404);
                 else respond(200, file_content)
@@ -23,8 +22,6 @@ http.createServer(function(request, response) {
         
     function respond(http_status, file_content) {
         response.statusCode = http_status;
-        //response.setHeader('Content-Type', 'text/html');
         response.end(file_content);
     }
-
 }).listen(80, function(){ console.log('Node.js webserver is running!  [CTRL]+[C] to stop!') });
