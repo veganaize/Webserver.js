f0b8ad61bb3361194c1bf8fb4e3792dd60fe6a2a
veganaize <veganaize@users.noreply.github.com>
2020-09-14 22:54:50 -0700

    Handle query string

--- webserver.js
+++ webserver.js
@@ -2,18 +2,21 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(request, response) {
-    var prefixed_url = 'public_html'+ request.url;
-    fs.stat(prefixed_url, function(bad_url, url_stat) {
+    // Remove query string
+    var prefixed_path = 'public_html'+ request.url.slice(0,
+            (request.url.indexOf('?')+1 || request.url.length+1) - 1);
+    // Get info on path
+    fs.stat(prefixed_path, function(bad_url, url_stat) {
         if (bad_url) respond(404);
         else {
-            if (url_stat.isDirectory() && request.url.slice(-1) !== '/') {
-                response.setHeader('Location', request.url +'/');
+            if (url_stat.isDirectory() && prefixed_path.slice(-1) !== '/') {
+                response.setHeader('Location', prefixed_path.slice(11) +'/');
                 respond(301);
                 return;
             }
             
-            if (prefixed_url.slice(-1) === '/') prefixed_url += 'index.html';
-            fs.readFile(prefixed_url, function(bad_filename, file_content) {
+            if (prefixed_path.slice(-1) === '/') prefixed_path += 'index.html';
+            fs.readFile(prefixed_path, function(bad_filename, file_content) {
                 if (bad_filename) respond(404);
                 else respond(200, file_content)
             });
